7f8baf8f4b20fa41e275a5d63c1ccff50d3acdb0
Lindvni <lindvni@gmail.com>
2026-05-04 00:15:55 +0200

    PayloadBuilder & requestBuilder

--- src/test/java/commons/Paths.java
+++ src/test/java/commons/Paths.java
@@ -4,26 +4,5 @@ import org.json.simple.JSONObject;
 
 public class Paths {
 
-    public class userPayload{
-
-        public static JSONObject userLoginPayload(String email, String password){
-            JSONObject userLogin =new JSONObject();
-            userLogin.put("email", email);
-            userLogin.put("password",password);
-
-            return userLogin;
-        }
-
-        public static JSONObject registerUserPayload(String firstName, String lastName, String email, String password, String groupId){
-            JSONObject registerUser = new JSONObject();
-            registerUser.put("firstName",firstName);
-            registerUser.put("lastName",lastName);
-            registerUser.put("email",email);
-            registerUser.put("password",password);
-            registerUser.put("confirmPassword",password);
-            registerUser.put("groupId",groupId);
-
-            return registerUser;
-        }
-    }
+    public static final String BASE_URL = "https://ndosiautomation.co.za/";
 }
---
+++ src/test/java/payloadBuilder/UserPayload.java
@@ -0,0 +1,27 @@
+package payloadBuilder;
+
+import org.json.simple.JSONObject;
+
+public class UserPayload {
+
+        public static JSONObject userLoginPayload(String email, String password){
+            JSONObject userLogin =new JSONObject();
+            userLogin.put("email", email);
+            userLogin.put("password",password);
+
+            return userLogin;
+        }
+
+        public static JSONObject registerUserPayload(String firstName, String lastName, String email, String password, String groupId){
+            JSONObject registerUser = new JSONObject();
+            registerUser.put("firstName",firstName);
+            registerUser.put("lastName",lastName);
+            registerUser.put("email",email);
+            registerUser.put("password",password);
+            registerUser.put("confirmPassword",password);
+            registerUser.put("groupId",groupId);
+
+            return registerUser;
+        }
+
+}
